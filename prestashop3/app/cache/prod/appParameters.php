338437498b444ed61d7b7ff12e0a97826f1705f7
Sandra <ptitbou.d_avril@hotmail.fr>
2017-12-01 09:21:53 +0100

    dossier presta eval du 30112017

---
+++ prestashop3/app/cache/prod/appParameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '51.255.196.206',
+    'database_port' => '',
+    'database_name' => 'crazychouwiPrestashop',
+    'database_user' => 'crazychouwi',
+    'database_password' => 'crazychouwi88659000',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'LSgX5uT6BApRFMyYx6270KmJrqmvw6jyMF9UaM38XQ70b47dBEGP2jAH',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-11-30',
+    'locale' => 'en-US',
+    'cookie_key' => 'uxeDoHPfhTGp00gwg76pY5B3BWYc0uAqs5uKdDoGeQWQ9skv2GmQietY',
+    'cookie_iv' => 'noCWSddf',
+    'new_cookie_key' => 'def00000aae6bf79851605637c74d5ae44b09d2d9c40701fe7e7bfe8d532dc9e340afdec251b5832b2ff621aebe9b87fec2bedc0a84f05745882d0ebc27a0fa34be5157d',
+  ),
+);
